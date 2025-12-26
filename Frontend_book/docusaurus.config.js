ea2b66f5aa9f9a8ccca1b3ed6c696444d10db042
Maha Meher <mahameherrr@gmail.com>
2025-12-26 20:08:32 +0500

    Fix Docusaurus config for Vercel

--- Frontend_book/docusaurus.config.js
+++ Frontend_book/docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://hackathon-1-book-84ym.vercel.app',
+  url: 'https://hackathon-1-book-chi.vercel.app',
   baseUrl: '/',
   trailingSlash: false,
 
